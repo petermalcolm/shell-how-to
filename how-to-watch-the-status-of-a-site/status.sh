142df6babe1e1a529bf5c657d5824cba5994d5cb
Peter Malcolm <peter.t.malcolm@gmail.com>
2026-05-15 16:14:05 -0400

    multiline search and example.org for status.sh

--- how-to-watch-the-status-of-a-site/status.sh
+++ how-to-watch-the-status-of-a-site/status.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 while true; do
-  curl -s -o /dev/null -w "%{http_code}" https://pt2.abu.org/auth/login
+  curl -s -o /dev/null -w "%{http_code}" https://example.org/login
   echo 
   sleep 30
 done
